e011ca7b6b7235d5a853e69b02b8a08dc9b14cb1
Dan Blossom <daniel.blossom1@marist.edu>
2019-03-28 11:15:20 -0400

    can put processes into table - GEFN

--- gui.py
+++ gui.py
@@ -6,11 +6,12 @@ Created on Mar 27, 2019
 
 from Processes import Processes
 import sys
+import time
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QApplication, QDialog
 from PyQt5.uic import loadUi
-from PyQt5.uic.Compiler.qtproxies import QtWidgets
-from PyQt5.Qt import QTableWidget, QVBoxLayout
+from PyQt5.uic.Compiler.qtproxies import QtWidgets, QtGui
+from PyQt5.Qt import QTableWidget, QTableWidgetItem
 
 class gui(QDialog):
     def __init__(self):
@@ -19,10 +20,22 @@ class gui(QDialog):
         self.setWindowTitle('PerfMon')
         self.setFixedSize(self.size())
         
-        
-        
-        
+    def updateGui(self):
+        proc = Processes()
+
+        #while True:
+        processesTuple = proc.collectProcesses()
+        #time.sleep(10)      
+        row = 0
+        for process in processesTuple:
+            for col in range(0,self.tableWidget.columnCount()):
+                self.tableWidget.insertRow(self.tableWidget.rowCount())
+                self.tableWidget.setItem(row,col,QTableWidgetItem(str(process[col])))
+            row = row + 1
+             
 app = QApplication(sys.argv)
 widget = gui()
+widget.updateGui()
 widget.show()
+widget.updateGui()
 sys.exit(app.exec_())
