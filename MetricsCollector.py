e0c4d7276e1e766dde2f4d86da119f2fab171ad8
Dan Blossom <daniel.blossom1@marist.edu>
2019-04-10 19:42:27 -0400

    first commit in branch

--- MetricsCollector.py
+++ MetricsCollector.py
@@ -46,8 +46,10 @@ class MetricsCollector:
             print(mount[1],mount[2]) # prints the mount point
             print(psutil.disk_usage(mount[1])[2]) # prints disk free space
         
-proc = Processes()
+#proc = Processes()
 
-while True:
-    proc.updateDatabase()
-    time.sleep(10)
+metric = MetricsCollector()
+
+#while True:
+#    proc.updateDatabase()
+#    time.sleep(10)
