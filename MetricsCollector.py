95c76fa8c8056c3671cf54ffe80e83911617b980
Dan Blossom <daniel.blossom1@marist.edu>
2019-04-13 12:28:49 -0400

    removed all collection code from MetricsCollector.py so it will only act as a driver file / entry point for program. Did leave some code commented out just for reference but must be removed before merge to master.

--- MetricsCollector.py
+++ MetricsCollector.py
@@ -12,16 +12,18 @@ class MetricsCollector:
     
     def __init__(self):
         cpuPercent = Cpu.getOverallCPUPercent(self)
+    #    self.stats()
+    #    self.diskinfo()
             
-    def stats(self):
-        stats = psutil.cpu_stats()
-        print(stats)
+    #def stats(self):
+    #    stats = psutil.cpu_stats()
+    #    print(stats)
         
-    def diskinfo(self):
-        print(psutil.disk_partitions())
-        for mount in psutil.disk_partitions():
-            print(mount[1],mount[2]) # prints the mount point
-            print(psutil.disk_usage(mount[1])[2]) # prints disk free space
+    #def diskinfo(self):
+    #    print(psutil.disk_partitions())
+    #    for mount in psutil.disk_partitions():
+    #        print(mount[1],mount[2]) # prints the mount point
+    #        print(psutil.disk_usage(mount[1])[2]) # prints disk free space
         
 #proc = Processes()
 
