db5fa97b62a30426f5bf7622d2e2006b1456cd65
Dan Blossom <daniel.blossom1@marist.edu>
2019-04-25 21:56:39 -0400

    added check to Pmtils.py which returns none for now (maybe want to return something else? Also added some tests for it in BackendTest.py. That adds to issue #27 and closes #31 for now

--- BackendTest.py
+++ BackendTest.py
@@ -1,9 +1,15 @@
 from Cpu import Cpu
+from Pmutils import Pmutils
 
+# CPU Tests
 cpu = Cpu()
 print(cpu.getCpuCount("bad info"))
 print(cpu.getCpuCount(True))
 print(cpu.getCpuCount(False))
 print(cpu.getCpuCount())
 
-
+#Pmtutils Tests
+pmu = Pmutils()
+print(pmu.convertBytes(4444.23432))
+print(pmu.convertBytes("not a number"))
+print(pmu.convertBytes(1)) 
--- Pmutils.py
+++ Pmutils.py
@@ -8,15 +8,16 @@ class Pmutils:
     
     @staticmethod
     def convertBytes(number):
-        symbols = ('K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y')
-        prefix = {}
-        for i, s in enumerate(symbols):
-            prefix[s] = 1 << (i + 1) * 10
-        for s in reversed(symbols):
-            if number >= prefix[s]:
-                value = float(number) / prefix[s]
-                return '%.1f%s' % (value, s)
-        return "%sB" % number
+        if isinstance(number, float):
+            symbols = ('K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y')
+            prefix = {}
+            for i, s in enumerate(symbols):
+                prefix[s] = 1 << (i + 1) * 10
+            for s in reversed(symbols):
+                if number >= prefix[s]:
+                    value = float(number) / prefix[s]
+                    return '%.1f%s' % (value, s)
+            return "%sB" % number
     
     @staticmethod
     def createTimeStamp():
