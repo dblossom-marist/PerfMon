cbae55d5bb5e6cdfad329679898c23cbaf5760dd
Dan Blossom <daniel.blossom1@marist.edu>
2019-04-27 10:27:28 -0400

    comments added to metricscollector.py

--- MetricsCollector.py
+++ MetricsCollector.py
@@ -1,20 +1,24 @@
 '''
-Created on Mar 8, 2019
-
+A classes used as a driver to run the needed classes/methods
+in the backend writing system information to a SQLite database
 @author: blossom
 '''
 
+# Import from Python lib
+import time
+# Import from classes created for this project.
 from Processes import Processes
 from Cpu import Cpu
 from Memory import Memory
-from Pmutils import Pmutils
-import time
 
 class MetricsCollector:
     
     def __init__(self):
         pass
     
+    '''
+    A static method that does everything, the 'driver'
+    '''
     @staticmethod
     def run():
         cpu = Cpu()
@@ -23,8 +27,8 @@ class MetricsCollector:
         mem.updateDatabase()
         proc = Processes()
         proc.updateDatabase()
-        
+
+# Run in a loop every minute.
 while True:
-    print(Pmutils.createTimeStamp()) # for debug
     MetricsCollector.run()
     time.sleep(60)
